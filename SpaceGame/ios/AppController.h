8648e92c3609018c5157955451d2da61a2459a76
HuuHieu <hieudh55@gmail.com>
2013-07-25 15:00:04 +0700

    Initial Commit

---
+++ SpaceGame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  SpaceGameAppController.h
+//  SpaceGame
+//
+//  Created by MAC on 7/25/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ SpaceGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SpaceGameAppController.h
+//  SpaceGame
+//
+//  Created by MAC on 7/25/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
